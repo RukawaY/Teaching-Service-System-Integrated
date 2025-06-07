e230c73b444a83e4e550ec542b913dbe1eb6cfed
TommyTay0712 <3939993584@qq.com>
2025-06-07 17:11:22 +0800

    Update ResourceRowMapper.java

--- src/main/java/com/Main/RowMapper/rss/ResourceRowMapper.java
+++ src/main/java/com/Main/RowMapper/rss/ResourceRowMapper.java
@@ -1,25 +1,116 @@
-package com.Main.RowMapper.rss;
+package com.Main.dto.rss;
 
-import com.Main.entity.rss.Resource;
-import org.springframework.jdbc.core.RowMapper;
+import java.time.LocalDateTime;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+public class ResourceDTO {
+    private Long resourceId;
+    private Long directoryId;
+    private Long courseId;
+    private String resourceName;
+    private String resourceType;
+    private LocalDateTime uploadTime;
+    private String filePath;
+    private String description;
+    private Long uploaderId;
+    private String keywords;
 
-public class ResourceRowMapper implements RowMapper<Resource> {
-    @Override
-    public Resource mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Resource resource = new Resource();
-        resource.setResourceId(rs.getLong("resource_id"));
-        resource.setUploaderId(rs.getLong("uploader_id"));
-        resource.setCourseId(rs.getLong("course_id"));
-        resource.setResourceName(rs.getString("resource_name"));
-        resource.setResourceType(rs.getString("resource_type"));
-        resource.setUploadTime(rs.getTimestamp("upload_time").toLocalDateTime());
-        resource.setFilePath(rs.getString("file_path"));
-        resource.setDescription(rs.getString("description"));
-        resource.setKeywords(rs.getString("keywords"));
-        resource.setDirectoryId(rs.getLong("directory_id"));
-        return resource;
+    // 无参构造函数
+    public ResourceDTO() {}
+
+    // 所有参数的构造函数
+    public ResourceDTO(Long resourceId, Long directoryId, Long courseId, String resourceName,
+                       String resourceType, LocalDateTime uploadTime, String filePath,
+                       String description, Long uploaderId, String keywords) {
+        this.resourceId = resourceId;
+        this.directoryId = directoryId;
+        this.courseId = courseId;
+        this.resourceName = resourceName;
+        this.resourceType = resourceType;
+        this.uploadTime = uploadTime;
+        this.filePath = filePath;
+        this.description = description;
+        this.uploaderId = uploaderId;
+        this.keywords = keywords;
+    }
+
+    // Getters and Setters
+    public Long getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(Long resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public Long getDirectoryId() {
+        return directoryId;
+    }
+
+    public void setDirectoryId(Long directoryId) {
+        this.directoryId = directoryId;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public LocalDateTime getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(LocalDateTime uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getUploaderId() {
+        return uploaderId;
+    }
+
+    public void setUploaderId(Long uploaderId) {
+        this.uploaderId = uploaderId;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
     }
 }
